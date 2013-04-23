05acbb068ff5b3c6ca0891b2cdaedebb0363c28e
José Tomás Albornoz <jojo@eljojo.net>
2013-04-23 11:28:01 +0200

    passing tests

--- lib/itsniluntilyoushipit.rb
+++ lib/itsniluntilyoushipit.rb
@@ -2,7 +2,8 @@ require "itsniluntilyoushipit/version"
 
 module Itsniluntilyoushipit
   class Isitnil
-
-
+    def is_it_nil?(options = {})
+      options[:shipped] && options[:shipped] == true
+    end
   end
 end
